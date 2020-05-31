c52d5756e79999c509ffaee4d646c9a83283ebc0
kuru7377kuru <warukyuwarukyu@gmail.com>
2020-05-31 15:26:34 +0900

    7act

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_145246) do
+ActiveRecord::Schema.define(version: 2020_05_30_181016) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_images_on_post_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_05_30_145246) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "images", "posts"
 end
