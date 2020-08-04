ae287e93ef9795b8ce3456f213e9c4d6fdc2cc3b
kuru7377kuru <warukyuwarukyu@gmail.com>
2020-08-05 04:42:07 +0900

    c

--- app/assets/javascripts/jQuery.js
+++ app/assets/javascripts/jQuery.js
@@ -1,3 +1,18 @@
 $(function() {
   $("h1").css("color", "green");
+  
+  var duration = 300;
+
+  $('.post').on("mouseover", function() {
+    $(this).find('.po').stop(true).animate({
+      bottom: "0px"
+    },duration)
+  });
+
+  $('.post').on("mouseout", function() {
+    $(this).find('.po').stop(true).animate({
+      bottom: "-100px"
+    },duration)
+  });
+
  });
